a95039c6f787130e0341de27b9eb063ec8a48145
wubin <1015378816@qq.com>
2018-02-28 14:18:31 +0800

    构建完整项目

---
+++ app/src/main/java/com/app/hubert/newbieguide/MainActivity.java
@@ -0,0 +1,105 @@
+package com.app.hubert.newbieguide;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.app.hubert.guide.core.Controller;
+import com.app.hubert.guide.NewbieGuide;
+import com.app.hubert.guide.listener.OnGuideChangedListener;
+import com.app.hubert.guide.listener.OnLayoutInflatedListener;
+import com.app.hubert.guide.listener.OnPageChangedListener;
+import com.app.hubert.guide.model.GuidePage;
+import com.app.hubert.guide.model.HighLight;
+import com.jaeger.library.StatusBarUtil;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = this.getClass().getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        StatusBarUtil.setTranslucent(this, 0);
+        setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.tv);
+        TextView tvBottom = (TextView) findViewById(R.id.tv_bottom);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GridViewActivity.start(MainActivity.this);
+            }
+        });
+        final Button button = (Button) findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TestFragmentActivity.start(MainActivity.this);
+            }
+        });
+
+        Animation enterAnimation = new AlphaAnimation(0f, 1f);
+        enterAnimation.setDuration(600);
+        enterAnimation.setFillAfter(true);
+
+        Animation exitAnimation = new AlphaAnimation(1f, 0f);
+        exitAnimation.setDuration(600);
+        exitAnimation.setFillAfter(true);
+
+        //新增多页模式，即一个引导层显示多页引导内容
+        NewbieGuide.with(this)
+                .setLabel("page")//设置引导层标示区分不同引导层，必传！否则报错
+                .setOnGuideChangedListener(new OnGuideChangedListener() {
+                    @Override
+                    public void onShowed(Controller controller) {
+                        Log.e(TAG, "NewbieGuide onShowed: ");
+                        //引导层显示
+                    }
+
+                    @Override
+                    public void onRemoved(Controller controller) {
+                        Log.e(TAG, "NewbieGuide  onRemoved: ");
+                        //引导层消失（多页切换不会触发）
+                    }
+                })
+                .setOnPageChangedListener(new OnPageChangedListener() {
+                    @Override
+                    public void onPageChanged(int page) {
+                        Log.e(TAG, "NewbieGuide  onPageChanged: " + page);
+                        //引导页切换，page为当前页位置，从0开始
+                    }
+                })
+                .alwaysShow(true)//是否每次都显示引导层，默认false，只显示一次
+                .addGuidePage(//添加一页引导页
+                        GuidePage.newInstance()//创建一个实例
+                                .addHighLight(button)//添加高亮的view
+                                .addHighLight(tvBottom, HighLight.Shape.RECTANGLE)
+                                .setLayoutRes(R.layout.view_guide)//设置引导页布局
+                                .setOnLayoutInflatedListener(new OnLayoutInflatedListener() {
+                                    @Override
+                                    public void onLayoutInflated(View view) {
+                                        //引导页布局填充后回调，用于初始化
+                                        TextView tv = view.findViewById(R.id.textView2);
+                                        tv.setText("我是动态设置的文本");
+                                    }
+                                })
+                                .setEnterAnimation(enterAnimation)//进入动画
+                                .setExitAnimation(exitAnimation)//退出动画
+                )
+                .addGuidePage(
+                        GuidePage.newInstance()
+                                .addHighLight(tvBottom, HighLight.Shape.RECTANGLE,20)
+                                .setLayoutRes(R.layout.view_guide_custom, R.id.iv)//引导页布局，点击跳转下一页或者消失引导层的控件id
+                                .setEverywhereCancelable(false)//是否点击任意地方跳转下一页或者消失引导层，默认true
+                                .setBackgroundColor(getResources().getColor(R.color.testColor))//设置背景色，建议使用有透明度的颜色
+                                .setEnterAnimation(enterAnimation)//进入动画
+                                .setExitAnimation(exitAnimation)//退出动画
+                )
+                .show();//显示引导层(至少需要一页引导页才能显示)
+    }
+}
